173b87fe0da9e9156878d541d9de145671f5b6c4
chanjeff2 <jeffchan142@gmail.com>
2020-02-27 00:12:35 +0800

    fix shrinkable header

--- src/script/basic_responsive_layout.js
+++ src/script/basic_responsive_layout.js
@@ -2,7 +2,7 @@
 
 let backToTopButton = document.getElementById("backToTop");
 let headerNav = document.getElementById("header");
-// let container = document.getElementById("container");
+let container = document.getElementById("container");
 backToTopButton.onclick = function() { backToTop() };
 window.onscroll = function () { whenScrollDown() };
 
@@ -19,13 +19,13 @@ function whenScrollDown() {
         backToTopButton.style.display = "block";
         if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
             // headerNav.style.transition = "0.2s";
-            headerNav.style.padding = "10px 20px";
+            headerNav.style.padding = "10px 0";
             headerNav.style.position = "fixed";
-            // container.style.marginTop = "100px";
+            container.style.marginTop = "400px";
         } else {
-            headerNav.style.padding = "160px 20px";
-            headerNav.style.position = "absolute";
-            // container.style.marginTop = "400px";
+            headerNav.style.padding = "160px 0";
+            headerNav.style.position = "relative";
+            container.style.marginTop = "0";
         }
     } else {
         backToTopButton.style.display = "none";
