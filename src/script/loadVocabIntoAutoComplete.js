d51d82af9217b5f5a43b41836d384cb45706dfc5
chanjeff2 <jeffchan142@gmail.com>
2020-02-29 21:46:03 +0800

    add check present feature

--- src/script/loadVocabIntoAutoComplete.js
+++ src/script/loadVocabIntoAutoComplete.js
@@ -1,33 +1,26 @@
 "use strict";
 
 document.body.onload = function() { main(); textField.focus(); };
-let vocabList = [];
+let tempVocabList = [];
 let rawVocabList = [];
 let textField = document.getElementById("textField");
+let hint = document.getElementById("hint_vocabList");
+let hintBox = document.getElementById("hintBox");
 let vocabListFile = [
-    "fall_vocab1.txt",
-    "fall_vocab2.txt",
-    "fall_vocab3.txt",
-    "spring_vocab1.txt",
-    "spring_vocab2.txt",
-    "spring_vocab3.txt"
+    "fall_vocab_list_1.txt",
+    "fall_vocab_list_2.txt",
+    "fall_vocab_list_3.txt",
+    "spring_vocab_list_1.txt",
+    "spring_vocab_list_2.txt",
+    "spring_vocab_list_3.txt"
 ];
-
-function loadVocabList(filename) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-             rawVocabList = xhttp.responseText.split("\n");
-            addVocabListToAutoComplete();
-        }
-    };
-    xhttp.open("GET", "/res/vocab/" + filename, true);
-    xhttp.send();
-}
+let vocabList = {};
+let i = 1;
+textField.oninput = function() { checkVocabInList() };
 
 function addVocabListToAutoComplete() {
-    vocabList = rawVocabList.filter(function(value) { return value });
-    vocabList.forEach(createNewAutoCompleteListObject)
+    tempVocabList = rawVocabList.filter(function(value) { return value });
+    tempVocabList.forEach(createNewAutoCompleteListObject)
 }
 
 function createNewAutoCompleteListObject(values) {
@@ -37,8 +30,52 @@ function createNewAutoCompleteListObject(values) {
     dataList.appendChild(newVocab)
 }
 
+function storeVocabList() {
+    vocabList[i] = tempVocabList;
+    i++;
+}
+
+function loadVocabList(filename) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+             rawVocabList = xhttp.responseText.split("\r\n");
+            addVocabListToAutoComplete();
+            storeVocabList()
+        }
+    };
+    xhttp.open("GET", "/res/vocab/" + filename, true);
+    xhttp.send();
+}
+
+
 function main() {
     for (let i of vocabListFile) {
         loadVocabList(i);
+    }
+}
+
+function isEmpty(obj) {
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+function checkVocabInList() {
+    let input = inputField.value;
+    let count = 0;
+    for (let key in vocabList) {
+        tempVocabList = vocabList[key].filter(function(value) { return value === input;});
+        if (!isEmpty(tempVocabList)) {
+            hint.innerHTML = vocabListFile[count];
+            hintBox.visbility = "visible";
+            return
+        } else {
+            hint.innerHTML = "";
+            hintBox.visbility = "hidden";
+        }
+        count++;
     }
 }
