57dcde7174175616cef64afd67726ee034e2d817
chanjeff2 <jeffchan142@gmail.com>
2020-03-29 02:40:42 +0800

    fix header

--- src/script/basic_responsive_layout.js
+++ src/script/basic_responsive_layout.js
@@ -15,19 +15,25 @@ function backToTop() {
 }
 
 function whenScrollDown() {
+    // show or hide display button
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         backToTopButton.style.display = "block";
-        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-            // headerNav.style.transition = "0.2s";
-            headerNav.style.padding = "10px 0";
-            headerNav.style.position = "fixed";
-            container.style.marginTop = "400px";
-        } else {
-            headerNav.style.padding = "160px 0";
-            headerNav.style.position = "relative";
-            container.style.marginTop = "0";
-        }
     } else {
         backToTopButton.style.display = "none";
+    }
+
+    // resizing header as scrolling
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        // headerNav.style.transition = "0.2s";
+        headerNav.style.padding = "10px 0";
+        headerNav.style.position = "fixed";
+        container.style.marginTop = "370px";
+    } else {
+        // headerNav.style.padding = "160px 0";
+        let paddingTop = Math.min(310, 160 + Math.max(document.body.scrollTop, document.documentElement.scrollTop));
+        let paddingBot = Math.max(10, 160 - Math.max(document.body.scrollTop, document.documentElement.scrollTop));
+        headerNav.style.padding = paddingTop.toString() + "px 0 " + paddingBot.toString() +"px";
+        headerNav.style.position = "relative";
+        container.style.marginTop = "0";
     }
 }
