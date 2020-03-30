b168f676b0a8bb0c1330a1198c563d95f1c5cdc8
chanjeff2 <jeffchan142@gmail.com>
2020-03-31 00:00:13 +0800

    fix autocomplete loading bug

--- src/script/loadVocabIntoAutoComplete.js
+++ src/script/loadVocabIntoAutoComplete.js
@@ -49,7 +49,7 @@ function loadVocabList(filename) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            rawVocabList = xhttp.responseText.split(/\n/);
+            rawVocabList = xhttp.responseText.split(/\n|\r\n/);
             addVocabListToAutoComplete();
             storeVocabList()
         }
