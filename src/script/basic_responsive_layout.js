3575dfeaf4cb67653503c79faff1c7dd5e48e08d
chanjeff2 <jeffchan142@gmail.com>
2020-09-02 00:17:12 +0800

    add theme toggle

---
+++ src/script/basic_responsive_layout.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let backToTopButton = document.getElementById("backToTop");
+let toggleThemeButton = document.getElementById("toggleTheme");
+let headerNav = document.getElementById("header");
+let container = document.getElementById("container");
+let theme = document.getElementById("theme");
+backToTopButton.onclick = function() { backToTop() };
+toggleThemeButton.onclick = function() { toggleTheme() };
+window.onscroll = function () { onScroll() };
+
+function backToTop() {
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    });
+    // backToTopButton.style.display = "none";
+}
+
+function toggleTheme() {
+    if (theme.href.endsWith("lightTheme.css")) {
+        theme.href = "src/stylesheets/darkTheme.css"
+    } else {
+        theme.href = "src/stylesheets/lightTheme.css"
+    }
+}
+
+function onScroll() {
+    // show or hide display button
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        backToTopButton.style.visibility = "visible";
+        toggleThemeButton.style.bottom = "90px";
+    } else {
+        backToTopButton.style.visibility = "hidden";
+        toggleThemeButton.style.bottom = "20px";
+    }
+
+    // resizing header as scrolling
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        headerNav.style.padding = "10px 0";
+        headerNav.style.position = "fixed";
+        container.style.marginTop = "370px";
+    } else {
+        // headerNav.style.padding = "160px 0";
+        let paddingTop = Math.min(310, 160 + Math.max(document.body.scrollTop, document.documentElement.scrollTop));
+        let paddingBot = Math.max(10, 160 - Math.max(document.body.scrollTop, document.documentElement.scrollTop));
+        headerNav.style.padding = paddingTop.toString() + "px 0 " + paddingBot.toString() +"px";
+        headerNav.style.position = "relative";
+        container.style.marginTop = "0";
+    }
+}
--- src/script_vocab/basic_responsive_layout.js
+++ src/script_vocab/basic_responsive_layout.js
@@ -1,60 +1,7 @@
 "use strict";
 
-let backToTopButton = document.getElementById("backToTop");
-let headerNav = document.getElementById("header");
-let container = document.getElementById("container");
 let loginBox = document.getElementById("loginBox");
 let loginBoxExpanded = true;
-backToTopButton.onclick = function() { backToTop() };
-window.onscroll = function () { whenScrollDown() };
-
-function backToTop() {
-    window.scroll({
-        top: 0,
-        behavior: "smooth"
-    });
-    // backToTopButton.style.display = "none";
-}
-
-function whenScrollDown() {
-    // show or hide display button
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
-
-    // resizing header as scrolling
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        // headerNav.style.transition = "0.2s";
-        headerNav.style.padding = "10px 0";
-        headerNav.style.position = "fixed";
-        container.style.marginTop = "370px";
-        // resize login block
-        if(!alreadyLogin) {
-            loginBox.style.height = "30px";
-            loginBox.style.width = "70px";
-            loginBox.style.display = "unset";
-            loginBox.style.cursor = "pointer";
-            loginBoxExpanded = false;
-        }
-    } else {
-        // headerNav.style.padding = "160px 0";
-        let paddingTop = Math.min(310, 160 + Math.max(document.body.scrollTop, document.documentElement.scrollTop));
-        let paddingBot = Math.max(10, 160 - Math.max(document.body.scrollTop, document.documentElement.scrollTop));
-        headerNav.style.padding = paddingTop.toString() + "px 0 " + paddingBot.toString() +"px";
-        headerNav.style.position = "relative";
-        container.style.marginTop = "0";
-        // resize login block
-        if(!alreadyLogin) {
-            loginBox.style.height = "330px";
-            loginBox.style.width = "300px";
-            loginBox.style.display = "flex";
-            loginBox.style.cursor = "default";
-            loginBoxExpanded = true;
-        }
-    }
-}
 
 function resizeLoginBlock() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
