1e78b25ff2992dbd5d2dfa278246b9cab9bccf2f
chanjeff2 <jeffchan142@gmail.com>
2020-12-26 04:55:43 +0800

    added media slide show along with navigator

--- admin/new_project/new_project.js
+++ admin/new_project/new_project.js
@@ -19,7 +19,6 @@ class Project {
 };
 
 function onUploadFile(input) {
-    console.log(input.files);
     let uploading = document.querySelector(".upload-message");
 
     if (input.files && input.files[0]) {
@@ -105,12 +104,10 @@ function generateProjectJSONString() {
     let videoLink = document.querySelector("#project-video-link").value;
     let videoID_regex = /v=([\d\w]+)|\.be\/([\d\w]+)/;
     let match = videoID_regex.exec(videoLink);
-    console.log(match);
     let videoID = "";
     if (match) {
         videoID = match[1] ?? match[2];
     }
-    console.log(videoID);
     newProject.videoLink = videoID ? "https://www.youtube.com/embed/" + videoID : "";
 
     // project link
@@ -134,24 +131,59 @@ function generateProjectPanel(proj_json_string) {
     let project_panel = document.createElement("div");
     project_panel.classList.toggle("project-panel");
 
+    let template = document.querySelector("#template-media-container");
+    let mediaContainerWrapper = template.content.cloneNode(true);
+    let project_media_container = mediaContainerWrapper.querySelector(".media-container");
+    let project_media_navigator_container = mediaContainerWrapper.querySelector(".navigator-container");
+    let templateNavigator = document.querySelector("#template-navigator");
     
+    if (project.image.length > 0 || project.videoLink) {
+        let counter = 0;
 
-    if (project.image.length > 0) {
-        let img = document.createElement("img");
+        // video
+        if (project.videoLink) {
+            let template = document.querySelector("#template-youtube-embedded-video");
+            let video = template.content.cloneNode(true).querySelector("iframe");
 
-        img.src = project.image[0];
-        img.classList.toggle("project-image");
+            video.src = project.videoLink;
+            video.classList.toggle("project-video");
+            video.dataset.index = counter;
 
-        img.alt = "project image";
-        project_panel.append(img);
-    } else if (project.videoLink) {
-        let template = document.querySelector("#youtube-embedded-video");
-        let video = template.content.cloneNode(true);
+            project_media_container.append(video);
 
-        video.querySelector("iframe").src = project.videoLink;
-        video.querySelector("iframe").classList.toggle("project-video");
+            let navigator = templateNavigator.content.cloneNode(true).querySelector(".navigator");
+            navigator.dataset.index = counter;
 
-        project_panel.append(video);
+            project_media_navigator_container.append(navigator);
+
+            ++counter;
+        }
+
+        // image
+        project.image.forEach(imageSrc => {
+            let img = document.createElement("img");
+
+            img.src = imageSrc;
+            img.classList.toggle("project-image");
+            img.alt = "project image";
+            img.dataset.index = counter;
+
+            project_media_container.append(img);
+
+            let navigator = templateNavigator.content.cloneNode(true).querySelector(".navigator");
+            navigator.dataset.index = counter;
+
+            project_media_navigator_container.append(navigator);
+
+            ++counter;
+        })
+
+        if (counter >= 2) {
+            project_media_container.classList.toggle("multiple-media");
+        }
+
+        project_media_container.children[2].classList.toggle("display");
+        project_media_navigator_container.children[0].classList.toggle("display");
     } else {
         let img = document.createElement("img");
 
@@ -160,8 +192,10 @@ function generateProjectPanel(proj_json_string) {
         img.classList.toggle("project-image");
 
         img.alt = "project image";
-        project_panel.append(img);
+        project_media_container.append(img);
     }
+
+    project_panel.append(mediaContainerWrapper);
 
     let project_details = document.createElement("div");
     project_details.classList.toggle("project-details");
@@ -191,7 +225,7 @@ function generateProjectPanel(proj_json_string) {
         flexbox.classList.toggle("icons-container");
 
         if (project.projectLink) {
-            let template = document.querySelector("#project-anchor-icon");
+            let template = document.querySelector("#template-project-anchor-icon");
             let project = template.content.cloneNode(true);
             project.querySelector("a").href = project.projectLink;
     
@@ -199,7 +233,7 @@ function generateProjectPanel(proj_json_string) {
         }
 
         if (project.githubLink) {
-            let template = document.querySelector("#github-anchor-icon");
+            let template = document.querySelector("#template-github-anchor-icon");
             let github = template.content.cloneNode(true);
             github.querySelector("a").href = project.githubLink;
     
@@ -207,7 +241,7 @@ function generateProjectPanel(proj_json_string) {
         }
     
         if (project.downloadLink) {
-            let template = document.querySelector("#download-anchor-icon");
+            let template = document.querySelector("#template-download-anchor-icon");
             let download = template.content.cloneNode(true);
             download.querySelector("a").href = project.downloadLink;
     
@@ -220,4 +254,153 @@ function generateProjectPanel(proj_json_string) {
     project_panel.append(project_details);
 
     return project_panel;
+}
+
+function previousMedia(node) {
+    let container = node.parentNode;
+    let noOfMedia = container.childElementCount - 2;
+
+    node.parentNode.children[0].disabled = true;
+    node.parentNode.children[1].disabled = true;
+
+    let currentMedia = container.querySelector(".display");
+    let index = currentMedia.dataset.index;
+
+    let previousIndex = 2 + (((+index - 1) % noOfMedia) + noOfMedia) % noOfMedia;
+    let previousMedia = container.children[previousIndex];
+
+    let navigatorContainer = container.parentNode.querySelector(".navigator-container");
+    let currentNavigator = navigatorContainer.querySelector(".display");
+    let nextNavigator = navigatorContainer.querySelector("[data-index=\"" + (previousIndex - 2) + "\"]");
+    currentNavigator.classList.toggle("display");
+    nextNavigator.classList.toggle("display");
+
+    setTimeout(() => {
+        currentMedia.classList.toggle("slide-out-right");
+    }, 0);
+
+    setTimeout(() => {
+        previousMedia.classList.toggle("display");
+        previousMedia.classList.toggle("slide-in-left");
+    }, 0);
+
+    setTimeout(() => {
+        currentMedia.classList.toggle("display");
+        currentMedia.classList.toggle("slide-out-right");
+        previousMedia.classList.toggle("slide-in-left");
+
+        container.children[0].disabled = false;
+        container.children[1].disabled = false;
+    }, 300);
+}
+
+function nextMedia(node) {
+    let container = node.parentNode;
+    let noOfMedia = container.childElementCount - 2;
+
+    container.children[0].disabled = true;
+    container.children[1].disabled = true;
+
+    let currentMedia = container.querySelector(".display");
+    let index = currentMedia.dataset.index;
+
+    let nextIndex = 2 + (((+index + 1) % noOfMedia) + noOfMedia) % noOfMedia;
+    let nextMedia = container.children[nextIndex];
+
+    let navigatorContainer = container.parentNode.querySelector(".navigator-container");
+    let currentNavigator = navigatorContainer.querySelector(".display");
+    let nextNavigator = navigatorContainer.querySelector("[data-index=\"" + (nextIndex - 2) + "\"]");
+    
+    currentNavigator.classList.toggle("display");
+    nextNavigator.classList.toggle("display");
+
+    setTimeout(() => {
+        currentMedia.classList.toggle("slide-out-left");
+    }, 0);
+
+    setTimeout(() => {
+        nextMedia.classList.toggle("display");
+        nextMedia.classList.toggle("slide-in-right");
+    }, 0);
+
+    setTimeout(() => {
+        currentMedia.classList.toggle("display");
+        currentMedia.classList.toggle("slide-out-left");
+        nextMedia.classList.toggle("slide-in-right");
+
+        container.children[0].disabled = false;
+        container.children[1].disabled = false;
+    }, 300);
+}
+
+function gotoMedia(navigator) {
+    let navigatorContainer = navigator.parentNode;
+
+    let currentNavigator = navigatorContainer.querySelector(".display");
+    let index = currentNavigator.dataset.index;
+    let nextIndex = navigator.dataset.index;
+
+    if (index == nextIndex) {
+        return;
+    }
+
+    currentNavigator.classList.toggle("display");
+    navigator.classList.toggle("display");
+
+    let container = navigatorContainer.parentNode.querySelector(".media-container");
+
+    container.children[0].disabled = true;
+    container.children[1].disabled = true;
+    navigatorContainer.querySelectorAll(".navigator").forEach( btn => {
+        btn.disabled = true;
+    });
+
+    let currentMedia = container.querySelector(".display");
+
+    let nextMedia = container.querySelector("[data-index=\"" + nextIndex + "\"]");
+
+    if (nextIndex > index) {
+        // go to right, next
+        setTimeout(() => {
+            currentMedia.classList.toggle("slide-out-left");
+        }, 0);
+    
+        setTimeout(() => {
+            nextMedia.classList.toggle("display");
+            nextMedia.classList.toggle("slide-in-right");
+        }, 0);
+    
+        setTimeout(() => {
+            currentMedia.classList.toggle("display");
+            currentMedia.classList.toggle("slide-out-left");
+            nextMedia.classList.toggle("slide-in-right");
+    
+            container.children[0].disabled = false;
+            container.children[1].disabled = false;
+            navigatorContainer.querySelectorAll(".navigator").forEach( btn => {
+                btn.disabled = false;
+            });
+        }, 300);
+    } else {
+        setTimeout(() => {
+            currentMedia.classList.toggle("slide-out-right");
+        }, 0);
+    
+        setTimeout(() => {
+            nextMedia.classList.toggle("display");
+            nextMedia.classList.toggle("slide-in-left");
+        }, 0);
+    
+        setTimeout(() => {
+            currentMedia.classList.toggle("display");
+            currentMedia.classList.toggle("slide-out-right");
+            nextMedia.classList.toggle("slide-in-left");
+    
+            container.children[0].disabled = false;
+            container.children[1].disabled = false;
+            navigatorContainer.querySelectorAll(".navigator").forEach( btn => {
+                btn.disabled = false;
+            });
+        }, 300);
+    }
 }
