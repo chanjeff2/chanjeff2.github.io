db769c1bea355247a4a06e0e9c484e225d9c0afd
chanjeff2 <jeffchan142@gmail.com>
2021-01-03 00:48:41 +0800

    toggle snow effect in firebase

--- common/floating_tools/snow.js
+++ common/floating_tools/snow.js
@@ -1,73 +1,84 @@
 "use strict";
 
+window.addEventListener("DOMContentLoaded", () => {
+    let db = firebase.firestore();
+    db.collection("admin").doc("setting").get().then(response => {
+        let setting = response.data();
+        if (setting.snow == false) {
+            return;
+        }
+        let snow = document.createElement("div");
+        snow.id = "snow";
+        snow.style.display = "block";
+        document.body.append(snow);
 
-let snow = document.createElement("div");
-snow.id = "snow";
-snow.style.display = "block";
-document.body.append(snow);
+        var script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+        script.onload = function(){
+            particlesJS("snow", {
+                "particles": {
+                    "number": {
+                        "value": 200,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
+                        }
+                    },
+                    "color": {
+                        "value": "#ffffff"
+                    },
+                    "opacity": {
+                        "value": 0.7,
+                        "random": false,
+                        "anim": {
+                            "enable": false
+                        }
+                    },
+                    "size": {
+                        "value": 5,
+                        "random": true,
+                        "anim": {
+                            "enable": false
+                        }
+                    },
+                    "line_linked": {
+                        "enable": false
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 5,
+                        "direction": "bottom",
+                        "random": true,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": true,
+                            "rotateX": 300,
+                            "rotateY": 1200
+                        }
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": false
+                        },
+                        "onclick": {
+                            "enable": false
+                        },
+                        "resize": false
+                    }
+                },
+                "retina_detect": true
+            });
+        }
+        document.head.append(script);
 
-var script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
-script.onload = function(){
-    particlesJS("snow", {
-        "particles": {
-            "number": {
-                "value": 200,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "opacity": {
-                "value": 0.7,
-                "random": false,
-                "anim": {
-                    "enable": false
-                }
-            },
-            "size": {
-                "value": 5,
-                "random": true,
-                "anim": {
-                    "enable": false
-                }
-            },
-            "line_linked": {
-                "enable": false
-            },
-            "move": {
-                "enable": true,
-                "speed": 5,
-                "direction": "bottom",
-                "random": true,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": true,
-                    "rotateX": 300,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": false
-                },
-                "onclick": {
-                    "enable": false
-                },
-                "resize": false
-            }
-        },
-        "retina_detect": true
-    });
-}
-document.head.append(script);
+        let snowBtn = document.querySelector("#snowBtn");
+        snowBtn.hidden = false;
+    })
+})
 
 function toggleSnowEffect() {
     let snow = document.querySelector("#snow");
